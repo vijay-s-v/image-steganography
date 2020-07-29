c2f0e87bd9da27c34e504c89e54bbe967bc07020
Dainn <vijay.s.venkatesan@protonmail.com>
2020-07-29 16:38:20 -0400

    Testing to see if file output from program matches xxd binary dump

--- steganographer.cpp
+++ steganographer.cpp
@@ -7,12 +7,15 @@ using namespace std;
 // Embed a hidden message into a given image
 int main(int argc, char * argv []){
 	char c;
+	int lineCounter = 1;
 	ifstream f("owl.png", ios::binary | ios::in);
 	while(f.get(c)){
 		for(int i = 7; i >= 0; i--){
 			cout << ((c >> i) & 1);
 		}
 		cout << " ";
+		if(lineCounter % 6 == 0){ cout << endl; }
+		lineCounter++;
 	}
 	return 0;
 }
